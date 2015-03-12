479be48d4272fcf7eccf768f974b4707bda9c3d5
Florian Gagnadre <floriangagnadre@Florians-MacBook-Pro.local>
2015-03-12 20:53:43 +0100

    Save results in file

--- capacite_garantie.py
+++ capacite_garantie.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+def get_groupe_ordonnancement(serveurs, P, R):
+    groupes = [([],[0 for i in range(R)]) for i in range(P)]
+    serveurs_sorted_by_capacite = sorted(serveurs,key=lambda s:-s[2])
+
+    for i, s in enumerate(serveurs_sorted_by_capacite):
+        g = select_group(groupes,s)
+        add_in_group(groupes, serveurs_sorted_by_capacite, g, s, i)
+    serveurs_updated = sorted(serveurs_sorted_by_capacite,key=lambda s:s[0])
+    return serveurs_updated
+
+
 def add_in_group(groupes, serveurs, g, s, i):
     groupes[g][0].append(s[0]) # ajoute l id du serveur a la liste des serveurs du groupe
     groupes[g][1][s[3]]+=s[2] # ajoute la capacite dans la liste des caoacites par rangees
@@ -30,17 +41,17 @@ def affiche_resultat(serveurs):
     for s in serveurs:
         print(str(s[3])+" "+str(s[4])+" "+str(s[5]))
 
+def save_results_in_file(serveurs, filename):
+    with open(filename,"w") as file:
+        for s in serveurs:
+            file.write(str(s[3])+" "+str(s[4])+" "+str(s[5])+"\n")
+
 if __name__ == "__main__":
     P = 2
     R = 5
     serveurs = [(1,1,1,2,0),(0,3,2,0,0),(0,3,3,1,0)]
-    groupes = [([],[0 for i in range(R)]) for i in range(P)]
-    serveurs_sorted_by_capacite = sorted(serveurs,key=lambda s:-s[2])
-
-    for i, s in enumerate(serveurs_sorted_by_capacite):
-        g = select_group(groupes,s)
-        add_in_group(groupes, serveurs_sorted_by_capacite, g, s, i)
-    serveurs = sorted(serveurs_sorted_by_capacite,key=lambda s:s[0])
-    affiche_resultat(serveurs)
+    serveurs_updated = get_groupe_ordonnancement(serveurs, P, R)
+    affiche_resultat(serveurs_updated)
+    save_results_in_file(serveurs_updated,"resultats.txt")
 
 
