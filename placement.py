7983abb1e459e7d067adc5762a2c027493e608c3
Stanislas Bernard <bernardstanislas@gmail.com>
2015-03-12 20:01:56 +0100

    working server indexes

--- placement.py
+++ placement.py
@@ -1,3 +1,10 @@
-from thierry import parse
+import parser
+import rawData
 
-serverList = parse()
+rawServerList = rawData.getRawServerList()
+serverList = parser.parseInd(rawServerList)
+
+
+#rowList = [(0, []),(1, []),(2, []),(3, []),(4, []),(5, []),(6, []),(7, []),(8, []),(9, []),(10, []),(11, []),(12, []),(13, []),(14, []),(15, []),]
+
+# print serverList
