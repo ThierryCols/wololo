d3033c8accdd35c6b7b951f204ab8cd6f3565875
Florian Gagnadre <floriangagnadre@Florians-MacBook-Pro.local>
2015-03-12 20:22:30 +0100

    merge avec Xavier

--- capacite_garantie.py
+++ capacite_garantie.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
+P = 45
+R = 16
 
-def select_groupe(groupes):
+serveurs = [(1,1,1,2,0),(0,3,2,0,0)]
+groupes = [([],[0 for i in range(R)]) for i in range(P)]
+
+serveurs_sorted_by_capacite = sorted(serveurs,key=lambda s:-s[2])
+
+def add_in_group(groupes, g, s):
+    groupes[g][0].append(s[0]) # ajoute l id du serveur a la liste des serveurs du groupe
+    groupes[g][1][s[3]]+=s[2] # ajoute la capacite dans la liste des caoacites par rangees
+
+def select_groupe(groupes,s):
     return capa_garantie(groupes)[1]
 
 
@@ -21,4 +32,9 @@ def capa(groupe):
     rangee_max = max(rangees_capacites)
     capa_min_groupe = sum(rangees_capacites) - rangee_max
     return capa_min_groupe
+
+if __name__ == "__main__":
+    for s in serveurs_sorted_by_capacite:
+        g = select_group(groupes,s)
+        add_in_group(groupes, g, s)
 
