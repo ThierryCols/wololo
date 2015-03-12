99dfa99df28cc84bef1ea3a9705b5f0eafe7bb3b
Florian Gagnadre <floriangagnadre@Florians-MacBook-Pro.local>
2015-03-12 20:47:55 +0100

    Fix bugs

--- capacite_garantie.py
+++ capacite_garantie.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 
-P = 45
-R = 16
-
-serveurs = [(1,1,1,2,0),(0,3,2,0,0)]
-groupes = [([],[0 for i in range(R)]) for i in range(P)]
-
-serveurs_sorted_by_capacite = sorted(serveurs,key=lambda s:-s[2])
-
-def add_in_group(groupes, g, s):
+def add_in_group(groupes, serveurs, g, s, i):
     groupes[g][0].append(s[0]) # ajoute l id du serveur a la liste des serveurs du groupe
     groupes[g][1][s[3]]+=s[2] # ajoute la capacite dans la liste des caoacites par rangees
+    serveurs[i] = (s[0],s[1],s[2],s[3],s[4],g) # ajoute le groupe au tuple representant le serveur
 
 def select_group(groupes,s):
-    return capa_garantie(groupes)[1]
+    return capa_garantie(groupes)[0]
 
 
 def capa_garantie(groupes):
@@ -33,8 +26,21 @@ def capa(groupe):
     capa_min_groupe = sum(rangees_capacites) - rangee_max
     return capa_min_groupe
 
+def affiche_resultat(serveurs):
+    for s in serveurs:
+        print(str(s[3])+" "+str(s[4])+" "+str(s[5]))
+
 if __name__ == "__main__":
-    for s in serveurs_sorted_by_capacite:
+    P = 2
+    R = 5
+    serveurs = [(1,1,1,2,0),(0,3,2,0,0),(0,3,3,1,0)]
+    groupes = [([],[0 for i in range(R)]) for i in range(P)]
+    serveurs_sorted_by_capacite = sorted(serveurs,key=lambda s:-s[2])
+
+    for i, s in enumerate(serveurs_sorted_by_capacite):
         g = select_group(groupes,s)
-        add_in_group(groupes, g, s)
+        add_in_group(groupes, serveurs_sorted_by_capacite, g, s, i)
+    serveurs = sorted(serveurs_sorted_by_capacite,key=lambda s:s[0])
+    affiche_resultat(serveurs)
+
 
