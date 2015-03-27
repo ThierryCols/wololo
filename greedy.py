96f5e342447b77ba64474626a1697ed6d4e0181d
Stanislas Bernard <bernardstanislas@gmail.com>
2015-03-27 21:08:50 +0100

    Lol naming conflict

---
+++ greedy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from inpute import *
+
+
+def greedLol():
+	allowedShapes = [(3,4), (4,3), (2,6), (6,2), (1,12), (12,1)]
+	inp = getInput()
+
+	# On parcourt la grande pizza
+	for x in inp:
+		for y in inp[x]
+			case = inp[x][y]
