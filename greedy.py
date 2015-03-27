5c6e38922ae49a3e5b2f50094091803bfcd3cdcc
Stanislas Bernard <bernardstanislas@gmail.com>
2015-03-27 21:11:34 +0100

    Fix mistakes lol sry

--- greedy.py
+++ greedy.py
@@ -5,8 +5,10 @@ from inpute import *
 def greedLol():
 	allowedShapes = [(3,4), (4,3), (2,6), (6,2), (1,12), (12,1)]
 	inp = getInput()
-
+	print(inp)
 	# On parcourt la grande pizza
-	for x in inp:
-		for y in inp[x]
-			case = inp[x][y]
+	# for x in inp:
+	# 	for y in inp[x]
+	# 		case = inp[x][y]
+
+greedLol()
