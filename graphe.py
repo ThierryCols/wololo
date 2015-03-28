16721a5b5db79690a30e3eb963a6efd8399abbba
Xavier <xajouppe@gmail.com>
2015-03-28 15:22:14 +0100

    fix

--- graphe.py
+++ graphe.py
@@ -16,7 +16,7 @@ class Graph:
         graphDict = {}
         for x in range(0, 75):
             for y in range(0, 300):
-                for z in range(0, 8):
+                for z in range(0, 9):
                     key = (x, y, z)
                     graphDict[key] = {}
                     graphDict[key]['x'] = x
@@ -31,7 +31,7 @@ class Graph:
         newZ = 0
         for altDelta in [-1, 0, 1]:
             newZ = z + altDelta
-            if newZ < 8 and newZ > 0:
+            if newZ < 9 and newZ > 0:
                 # vent
                 newCoordXY = self.applyWind(x, y, newZ)
                 newX = newCoordXY[0]
@@ -43,7 +43,7 @@ class Graph:
 
     def applyWind(self, x, y, z):
         C = 299
-        wind = self.data[z][x][y]
+        wind = self.data[z-1][x][y]
         X = x + wind[0]
         Y = y + wind[1]
         if X > 74 or X < 0:
