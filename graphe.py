66f3ee86eccbee6cd03e6f45bf82e9fb8d57d0d0
Xavier <xajouppe@gmail.com>
2015-03-28 12:54:14 +0100

    graphe fonctionnel

--- graphe.py
+++ graphe.py
@@ -1,47 +1,66 @@
 from donneesEntree import *
 
 
-class graph:
+class Graph:
 
     def __init__(self):
-        self.data = getInput()
-        self.graph = self.buildGraph(data)
+        self.data = getWinds()
+        self.graph = self.buildGraph()
+
+    def __str__(self):
+        return str(self.graph)
 
 
-# methodes de construction
-def buildGraph(self):
-    graphDict = {}
-    for x in range(0, 75):
-        for y in range(0, 5):
-            for z in range(0, 8):
-                key = str(x)+str(y)+str(z)
-                graphDict[key]['x'] = x
-                graphDict[key]['y'] = y
-                graphDict[key]['z'] = z
-                graphDict[key]['L_adj'] = self.getLAdj(x, y, z)
-    return graphDict
+    # methodes de construction
+    def buildGraph(self):
+        graphDict = {}
+        for x in range(0, 75):
+            for y in range(0, 300):
+                for z in range(0, 8):
+                    key = (x, y, z)
+                    graphDict[key] = {}
+                    graphDict[key]['x'] = x
+                    graphDict[key]['y'] = y
+                    graphDict[key]['z'] = z
+                    graphDict[key]['listeAdjacence'] = self.getLAdj(x, y, z)
+        return graphDict
 
 
-def getLAdj(self, x, y, z):
-    L = []
-    if z < 8:
-        L.append((x, y, z+1))
-    elif z > 0:
-        L.append((x, y, z-1))
-    w = self.wind(x, y, z)
-    L.append((x+w[0], y+w[1], z))
-    return L
+    def getLAdj(self, x, y, z):
+        L = []
+        newZ = 0
+        for altDelta in [-1, 0, 1]:
+            newZ = z + altDelta
+            if newZ < 8 and newZ >= 0:
+                # vent
+                newCoordXY = self.applyWind(x, y, newZ)
+                newX = newCoordXY[0]
+                newY = newCoordXY[1]
+                if newX != -1:
+                    L.append((newX, newY, newZ))
+        return L
 
 
-# methodes publiques
-def getPossibleMoves(self, x, y, z, graph):
-    key = str(x)+str(y)+str(z)
-    return graph[key]['L_adj']
+    def applyWind(self, x, y, z):
+        C = 299
+        wind = self.data[z][x][y]
+        X = x + wind[0]
+        Y = y + wind[1]
+        if X > 74 or X < 0:
+            X = -1
+        elif Y > C:
+            Y = Y - C
+        elif Y < 0:
+            Y = Y + C
+        return (X, Y)
 
 
-def wind(self, x, y, z):
-    return self.data[x][y][z]
+    # methodes publiques
+    def getPossibleMoves(self, x, y, z, graph):
+        key = (x, y, z)
+        return graph[key]['listeAdjacence']
 
 
 if __name__ == "__main__":
-    print(self.graph)
+    g = Graph()
+    print(g)
